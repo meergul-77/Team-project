618b9192abeaa78002d4986420e3cf680504470a
Meergul-77 <meka506772@gmail.com>
2021-02-05 12:00:02 +0600

    change routes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const jsonServer = require ('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
+
 const middlewares = jsonServer.defaults({
     static: './build'
 });
@@ -8,6 +9,9 @@ const middlewares = jsonServer.defaults({
 const PORT = process.env.PORT || 8000;
 
 server.use(middlewares);
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+}))
 server.use(router);
 
 server.listen(PORT, () => {
